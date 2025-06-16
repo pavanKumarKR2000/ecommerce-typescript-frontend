395954b50bd472214f4250063272a1e3a759cc33
Pavankumar K R <pavan123456pop@gmail.com>
2025-06-16 20:37:52 +0530

    change file name

---
+++ components/product/featured-products.tsx
@@ -0,0 +1,21 @@
+import { Product } from "@/types";
+import ProductCard from "./product-card";
+
+interface FeaturedProductsProps {
+  products: Product[];
+}
+
+const FeaturedProducts = ({ products }: FeaturedProductsProps) => {
+  return (
+    <div className="space-y-6 py-10">
+      <h2 className="font-bold text-2xl">Featured Products</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
