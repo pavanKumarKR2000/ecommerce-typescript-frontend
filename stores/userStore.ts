ad8ce94ed1417e6bdd47598b9e14f2ec3f63c127
Pavankumar K R <pavan123456pop@gmail.com>
2025-06-16 20:35:25 +0530

    add zustand stores folder

---
+++ stores/userStore.ts
@@ -0,0 +1,30 @@
+// src/stores/userStore.ts
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  role: "admin" | "user";
+}
+
+interface UserStore {
+  user: User | null;
+  setUser: (user: User) => void;
+  clearUser: () => void;
+}
+
+export const useUserStore = create<UserStore>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+      clearUser: () => set({ user: null }),
+    }),
+    {
+      name: "user-store", // key in localStorage
+      // storage: typeof window !== "undefined" ? localStorage : undefined,
+    },
+  ),
+);
