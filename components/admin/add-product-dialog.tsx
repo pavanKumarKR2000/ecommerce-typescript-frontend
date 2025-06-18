8072ad800fe1b0549eb4d0f236ca619ff75e4af2
Pavankumar K R <pavan123456pop@gmail.com>
2025-06-18 17:42:55 +0530

    add admin components

---
+++ components/admin/add-product-dialog.tsx
@@ -0,0 +1,31 @@
+"use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+import { useState } from "react";
+import AddProductForm from "./add-product-form";
+
+const AddProductDialog = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button>Add product</Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-md md:max-w-3xl">
+        <DialogHeader className="mt-4 space-y-6">
+          <DialogTitle>Add product</DialogTitle>
+          <AddProductForm />
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddProductDialog;
