8942ee71d775269e3beec724c61b0916b4669723
Pavankumar K R <pavan123456pop@gmail.com>
2025-06-19 17:16:25 +0530

    add actions related to products

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,6 +1,7 @@
 "use server";
 import { cookies } from "next/headers";
 import { api } from "../axios";
+import { revalidatePath } from "next/cache";
 export const getFeaturedProducts = async () => {
   try {
     const res = await api.get("/products/featured-products");
@@ -23,6 +24,77 @@ export const addProduct = async (product: any) => {
       withCredentials: true,
     });
 
+    revalidatePath("/admin/products");
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const updateProduct = async (id: number, product: any) => {
+  try {
+    const token = (await cookies()).get("accessToken")?.value;
+
+    const res = await api.put(`/products/${id}`, product, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      withCredentials: true,
+    });
+
+    revalidatePath("/admin/products");
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getProducts = async () => {
+  try {
+    const token = (await cookies()).get("accessToken")?.value;
+
+    const res = await api.get("/products", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      withCredentials: true,
+    });
+
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getProduct = async (id: number) => {
+  try {
+    const token = (await cookies()).get("accessToken")?.value;
+
+    const res = await api.get(`/products/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      withCredentials: true,
+    });
+
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const deleteProduct = async (id: number) => {
+  try {
+    const token = (await cookies()).get("accessToken")?.value;
+
+    const res = await api.delete(`/products/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      withCredentials: true,
+    });
+
+    revalidatePath("/admin/products");
     return res.data;
   } catch (err) {
     console.log(err);
