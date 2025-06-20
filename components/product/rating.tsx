48229aca4e23b426500c4b849c7797f497541f16
Pavankumar K R <pavan123456pop@gmail.com>
2025-06-20 11:30:54 +0530

    add rating component

---
+++ components/product/rating.tsx
@@ -0,0 +1,56 @@
+import {
+  IconStarHalfFilled,
+  IconStar,
+  IconStarFilled,
+} from "@tabler/icons-react";
+
+interface RatingProps {
+  rating: number;
+}
+
+const Rating = ({ rating }: RatingProps) => {
+  return (
+    <div className="flex items-center gap-2">
+      <div className="flex gap-1">
+        {rating >= 1 ? (
+          <IconStarFilled className="size-5" />
+        ) : rating >= 0.5 ? (
+          <IconStarHalfFilled className="size-5" />
+        ) : (
+          <IconStar className="size-5" />
+        )}
+        {rating >= 2 ? (
+          <IconStarFilled className="size-5" />
+        ) : rating >= 1.5 ? (
+          <IconStarHalfFilled className="size-5" />
+        ) : (
+          <IconStar className="size-5" />
+        )}
+        {rating >= 3 ? (
+          <IconStarFilled className="size-5" />
+        ) : rating >= 2.5 ? (
+          <IconStarHalfFilled className="size-5" />
+        ) : (
+          <IconStar className="size-5" />
+        )}
+        {rating >= 4 ? (
+          <IconStarFilled className="size-5" />
+        ) : rating >= 3.5 ? (
+          <IconStarHalfFilled className="size-5" />
+        ) : (
+          <IconStar className="size-5" />
+        )}
+        {rating >= 5 ? (
+          <IconStarFilled className="size-5" />
+        ) : rating >= 4.5 ? (
+          <IconStarHalfFilled className="size-5" />
+        ) : (
+          <IconStar className="size-5" />
+        )}
+      </div>
+      {rating === 0 && <p className="text-xs">No ratings yet</p>}
+    </div>
+  );
+};
+
+export default Rating;
